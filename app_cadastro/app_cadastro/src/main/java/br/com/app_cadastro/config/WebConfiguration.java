7698896a9120b6481326ef9f242bc6f815654ba0
bela-mad <isabelaargolo3110@gmail.com>
2022-06-25 23:02:59 -0300

    organizando de novo

---
+++ app_cadastro/app_cadastro/src/main/java/br/com/app_cadastro/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.app_cadastro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorParameter(false) //sempre especificara o formato padrao primeiro (json)
+		.ignoreAcceptHeader(false) //não vai ignorar o formato especificado no cabeçalho da requisiçao
+		.defaultContentType(MediaType.APPLICATION_JSON) //é o formato padrao
+		.mediaType("json",MediaType.APPLICATION_JSON) //é o formato suportado pela API
+		.mediaType("xml",MediaType.APPLICATION_XML);
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") //mapear todos
+		.allowedMethods("GET", "PUT", "POST", "DELETE", "PATH", "OPTIONS", "TRACE", "HEAD", "CONNECT");
+	}	
+}
